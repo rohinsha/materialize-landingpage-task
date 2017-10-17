46e053ac483f7b3e67e9838b08cc46bacef0a102
rohinsha <r2hinsha@gmail.com>
2017-10-17 14:28:58 +0530

    Mobile responsive

--- js/custom.js
+++ js/custom.js
@@ -1,38 +1,34 @@
 $(document).ready(function(){
-      
-      $('.carousel.carousel-slider').carousel({full_width: true});
+    $('.carousel.carousel-slider').carousel({full_width: true});
+    $('.collapsible').collapsible({
+       accordion:true
+    });
+  
+  $('.button-collapse-1').sideNav({
+      menuWidth: 300, 
+        closeOnClick: false 
+    });
 
-  $('.collapsible').collapsible({
-  	accordion:true
-  });
-$('.button-collapse-1').sideNav({
-      menuWidth: 300, // Default is 240
-      closeOnClick: false // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    }
-  );
-
-$('.button-collapse-2').sideNav({
-      menuWidth: 300, // Default is 240
-      closeOnClick: false,
-      edge:"right", // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    }
-  );
+  $('.button-collapse-2').sideNav({
+        menuWidth: 300, 
+        closeOnClick: false,
+        edge:"right",
+    });
+  
+  $('.button-collapse-3').sideNav({
+        menuWidth: 300, 
+        closeOnClick: false,
+        edge:"right",
+    });
  
-  		 $(".close-1").sideNav('hide');
+    $(".close-1").sideNav('hide');
 
-   $(".close-2").sideNav('hide');
-
-
+    $(".close-2").sideNav('hide');
+      $(".close-3").sideNav('hide');
+  });
       
-      });
-      
-
-
-
-      
-
-setInterval(function(){
-  $('.carousel').carousel('next');
-}, 5000);
+  setInterval(function(){
+      $('.carousel').carousel('next');
+  }, 5000);
 
  
